68c0dd67452a1b3ad40aabcc2fcb3ac9327a687d
svan23 <vansla>
2025-10-17 07:12:38 -0700

    Initial Commit

---
+++ Assignment_1/Assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1App.swift
+//  Assignment_1
+//
+//  Created by Vanessa La on 2025-10-17.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
